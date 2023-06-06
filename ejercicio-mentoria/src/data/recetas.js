313d5074996e967c44069d70143a07328575cea5
Àlex Rofes Galobardes <129336104+RofinhoOo@users.noreply.github.com>
2023-06-06 12:19:29 +0000

    sin acabar

---
+++ ejercicio-mentoria/src/data/recetas.js
@@ -0,0 +1,62 @@
+ export const recetas = [
+    { ID: 1,
+      imagen: "ruta-de-la-imagen1.jpg",
+      titulo: "Pastel de Chocolate",
+      tiempoPreparacion: "1 hora",
+      dificultad: "Media",
+      ingredientes: [
+        "200g de chocolate negro",
+        "200g de mantequilla",
+        "200g de azúcar",
+        "4 huevos",
+        "150g de harina",
+        "1 cucharadita de levadura en polvo",
+        "1 pizca de sal",
+      ],
+      pasos: [
+        "Precalienta el horno a 180°C.",
+        "Derrite el chocolate y la mantequilla en un recipiente al baño María.",
+        "En otro recipiente, bate los huevos con el azúcar hasta obtener una mezcla cremosa.",
+        "Añade la mezcla de chocolate y mantequilla derretidos a la mezcla de huevos y azúcar. Mezcla bien.",
+        "Tamiza la harina, la levadura en polvo y la sal sobre la mezcla de chocolate y huevos. Mezcla hasta obtener una masa homogénea.",
+        "Vierte la masa en un molde para pastel previamente engrasado.",
+        "Hornea durante aproximadamente 30-35 minutos, o hasta que un palillo insertado en el centro salga limpio.",
+        "Deja enfriar antes de desmoldar y servir.",
+      ],
+    },
+    { 
+        ID: 2,
+      imagen: "ruta-de-la-imagen2.jpg",
+      titulo: "Ensalada César",
+      tiempoPreparacion: "20 minutos",
+      dificultad: "Fácil",
+      ingredientes: [
+        "200g de lechuga romana",
+        "50g de queso parmesano rallado",
+        "100g de pan crujiente",
+        "100g de pechuga de pollo a la parrilla",
+        "2 cucharadas de salsa César",
+        "1 diente de ajo picado",
+        "1 cucharada de jugo de limón",
+        "Sal y pimienta al gusto",
+      ],
+      pasos: [
+        "Lava y corta la lechuga romana en trozos.",
+        "Corta el pan crujiente en cubos pequeños y dóralos en una sartén con un poco de aceite de oliva.",
+        "Corta la pechuga de pollo a la parrilla en tiras delgadas.",
+        "En un tazón grande, mezcla la lechuga, el pollo, el pan crujiente y el queso parmesano.",
+        "En otro recipiente, mezcla la salsa César, el ajo picado, el jugo de limón, la sal y la pimienta.",
+        "Vierte la salsa sobre la ensalada y mezcla bien para cubrir todos los ingredientes.",
+        "Sirve la ensalada en platos individuales y decora con un poco de queso parmesano rallado.",
+      ],
+    },
+    
+  ];
+  
+  
+  
+  
+  
+  
+  
+  
