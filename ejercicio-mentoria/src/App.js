a0ec936c202ac62d68592bbfa4615c854d4b4c51
Àlex Rofes Galobardes <129336104+RofinhoOo@users.noreply.github.com>
2023-06-15 11:58:36 +0000

    finalizado

--- ejercicio-mentoria/src/App.js
+++ ejercicio-mentoria/src/App.js
@@ -8,36 +8,19 @@ function App() {
   return (
     <div className="App">
       <h1>Recetas gastronómicas</h1>
-      <Receta
-        ID={recetas[0].ID}
-        image={recetas[0].image}
-        title={recetas[0].title}
-        difficulty={recetas[0].difficulty}
-        timeCoock={recetas[0].timeCoock}
-        ingredients={recetas[0].ingredients}
-        steps={recetas[0].steps}
-     
-      />
+      {recetas.map((receta)=>{
+        return(
+        <Receta key={receta.ID}
+        ID={receta.ID}
+        image={receta.image}
+        title={receta.title}
+        difficulty={receta.difficulty}
+        timeCoock={receta.timeCoock}
+        ingredients={receta.ingredients}
+        steps={receta.steps}
+      />)
+      })}
 
-      <Receta
-        ID={recetas[1].ID}
-        image={recetas[1].image}
-        title={recetas[1].title}
-        difficulty={recetas[1].difficulty}
-        timeCoock={recetas[1].timeCoock}
-        ingredients={recetas[1].ingredients}
-        steps={recetas[1].steps}
-      />
-
-      <Receta
-        ID={recetas[2].ID}
-        image={recetas[2].image}
-        title={recetas[2].title}
-        difficulty={recetas[2].difficulty}
-        timeCoock={recetas[2].timeCoock}
-        ingredients={recetas[2].ingredients}
-        steps={recetas[2].steps}
-      />
     </div>
   );
 }
